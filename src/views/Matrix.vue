63d57fc3b8e758dc6259083472d1b242f1c5df67
gordonstephen <12711372+gordonstephen@users.noreply.github.com>
2019-08-05 11:35:13 +0100

    Clean up formatting
    Replaced all tabs with spaces.

--- src/views/Matrix.vue
+++ src/views/Matrix.vue
@@ -1,28 +1,24 @@
 <template>
-  <div class=container>
+  <div class="container">
     <div class="row">
       <div class="col-md-12">
         <div class="home-content">
-         
-		<h5 class="mt-3">Matrix Selection</h5>
-		
-		<h6 class="mt-3">Available matrices:</h6>
+          <h5 class="mt-3">Matrix Selection</h5>
+
+          <h6 class="mt-3">Available matrices:</h6>
           <div>
             <b-form-select v-model="selected" :options="options" :select-size="4"></b-form-select>
             <b-card v-if="selected" no-body class="mt-3">
-              <b-card-header class="bg-secondary text-white">
-                {{selected.name}}
-              </b-card-header>
+              <b-card-header class="bg-secondary text-white">{{selected.name}}</b-card-header>
               <b-card-body>
                 <p>Description: {{selected.description}}</p>
                 <p>Last updated: {{selected.lastUpdated}}</p>
               </b-card-body>
             </b-card>
-            
-			<div  class="d-flex justify-content-center mb-3 mt-1">
-				<b-button  variant="primary" class="mt-3" @click="viewMatrix">View</b-button>
-				
-  		  </div>
+
+            <div class="d-flex justify-content-center mb-3 mt-1">
+              <b-button variant="primary" class="mt-3" @click="viewMatrix">View</b-button>
+            </div>
           </div>
         </div>
       </div>
@@ -32,79 +28,73 @@
 
 
 <script>
-import { mapGetters } from 'vuex'
-import brapi from '@solgenomics/brapijs';
+import { mapGetters } from "vuex";
+import brapi from "@solgenomics/brapijs";
 
-	export default {
-		data: function () {
-			return {
-				selected: null,
-				options: [],
-      }
-	},
+export default {
+  data: function() {
+    return {
+      selected: null,
+      options: []
+    };
+  },
 
-	computed: {
-		...mapGetters([
-			'baseUrl',
-			'authToken',
-			'selectedOptions',
-			'selectedStudyId'
-		])
-	},
+  computed: {
+    ...mapGetters([
+      "baseUrl",
+      "authToken",
+      "selectedOptions",
+      "selectedStudyId"
+    ])
+  },
 
-	mounted: function () {
-		
-		var vm = this
-		var brapiJs = brapi(this.baseUrl, "1.2", this.authToken)
+  mounted: function() {
+    var vm = this;
+    var brapiJs = brapi(this.baseUrl, "1.2", this.authToken);
 
-		if(this.selectedStudyId>0){
+    if (this.selectedStudyId > 0) {
+      var params = { studyDbId: this.selectedStudyId };
 
-			var params = { studyDbId: this.selectedStudyId}
-			
-			brapiJs.allelematrices(params).each((matrix) => {
-			vm.addOption(matrix)
-		})
-		}else {
-			brapiJs.allelematrices().each((matrix) => {
-			vm.addOption(matrix)
-		})
-		}
-	},
+      brapiJs.allelematrices(params).each(matrix => {
+        vm.addOption(matrix);
+      });
+    } else {
+      brapiJs.allelematrices().each(matrix => {
+        vm.addOption(matrix);
+      });
+    }
+  },
 
-	methods: {
-		addOption: function(option) {
-			var matrix = {
-				name: option.name,
-				id: option.matrixDbId,
-				studyId: option.studyDbId,
-				lastUpdated: option.lastUpdated
+  methods: {
+    addOption: function(option) {
+      var matrix = {
+        name: option.name,
+        id: option.matrixDbId,
+        studyId: option.studyDbId,
+        lastUpdated: option.lastUpdated
+      };
+      this.options.push({ text: matrix.name, value: matrix });
+    },
 
-			}
-			this.options.push({text: matrix.name, value: matrix})
-			
-		},
-		
-    	navigateToNextPage(){
+    navigateToNextPage() {
+      this.$router.push({ name: "options" });
+    },
 
-			this.$router.push({ name: 'options'});
-		
-		},
-		
-		addOption: function(option) {
-			var study = {
+    addOption: function(option) {
+      var study = {
         name: option.name,
         matrixName: option.matrixName,
-				description: option.description,
+        description: option.description,
         lastUpdated: option.lastUpdated,
-				dbId: option.matrixDbId
-			}
-			this.options.push({text: study.name, value: study})
+        dbId: option.matrixDbId
+      };
+      this.options.push({ text: study.name, value: study });
     },
-    
+
     viewMatrix: function() {
-      this.$store.dispatch('ON_MATRIX_ID_CHANGED', this.selected.dbId)
-      this.$router.push({ name: 'bytes'})
+      this.$store.dispatch("ON_MATRIX_ID_CHANGED", this.selected.dbId);
+      this.$router.push({ name: "bytes" });
     }
-	}
-}
+  }
+};
 </script>
