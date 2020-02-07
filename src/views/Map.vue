3798f7d0c2e7653ab2b561dda9e486a3314078ab
gordonstephen <12711372+gordonstephen@users.noreply.github.com>
2020-02-07 11:27:07 +0530

    Update layout of maps page

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,31 +1,38 @@
 <template>
-  <div>
-    <h5 class="mt-3">Map Selection</h5>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="home-content">
+          <h5 class="mt-3">Map Selection</h5>
 
-    <h6 class="mt-3">Available maps:</h6>
-    <b-form-select v-model="selectedMap" :options="options"></b-form-select>
-    <div></div>
+          <h6 class="mt-3">Available maps:</h6>
+          <b-form-select v-model="selectedMap" :options="options"></b-form-select>
+          <div></div>
 
-    <div v-if="selectedMap">
-      <b-card no-body class="mt-3">
-        <b-card-header class="bg-info text-white">Details: {{selectedMap.mapName}}</b-card-header>
-        <b-card-body>
-          <p>Map Id: {{selectedMap.mapDbId}}</p>
-          <p>Type: {{selectedMap.type}}</p>
-          <p>Unit: {{selectedMap.unit}}</p>
-          <p>Markers: {{selectedMap.markerCount}}</p>
-          <p>Chromosomes: {{selectedMap.linkageGroupCount}}</p>
-        </b-card-body>
-      </b-card>
-    </div>
-    <div class="d-flex justify-content-center mb-3 mt-1">
-      <b-button
-        id="continueBtn"
-        v-if="selectedMap"
-        size="sm"
-        variant="primary"
-        @click="navigateToNextPage"
-      >Select</b-button>
+          <div v-if="selectedMap">
+            <b-card no-body class="mt-3">
+              <b-card-header class="bg-info text-white">Details: {{selectedMap.mapName}}</b-card-header>
+              <b-card-body>
+                <p>Map Id: {{selectedMap.mapDbId}}</p>
+                <p>Type: {{selectedMap.type}}</p>
+                <p>Unit: {{selectedMap.unit}}</p>
+                <p>Markers: {{selectedMap.markerCount}}</p>
+                <p>Chromosomes: {{selectedMap.linkageGroupCount}}</p>
+              </b-card-body>
+            </b-card>
+          </div>
+          <div class="d-flex justify-content-center mb-3 mt-1">
+            <b-button id="skipBtn" variant="secondary" class="mt-3 mr-3" @click="navigateToNextPage">Skip</b-button>
+            <b-button
+              id="continueBtn"
+              :disabled="selectedMap === null"
+              variant="primary"
+              class="mt-3"
+              @click="selectAndNavigateToNextPage"
+            >Select</b-button>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -40,8 +47,7 @@ export default {
     return {
       selectedMap: null,
       options: [],
-      description: null,
-      isSomethingSelected: false
+      description: null
     };
   },
 
@@ -72,10 +78,13 @@ export default {
       this.options.push({ text: option.mapName, value: option });
     },
 
-    navigateToNextPage() {
-      this.$store.dispatch("ON_MAP_ID_CHANGED", this.selectedMap.mapDbId);
-      //move on to matrix. It always comes after maps.
+     navigateToNextPage() {
       this.$router.push({ name: "bytes" });
+    },
+
+    selectAndNavigateToNextPage() {
+      this.$store.dispatch("ON_MAP_ID_CHANGED", this.selectedMap.mapDbId);
+      this.navigateToNextPage();
     }
   }
 };
